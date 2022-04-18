e86b555acbcbf44c395a89cc095079bfb118f78a
andresxdxd25 <103850922+andresxdxd25@users.noreply.github.com>
2022-04-18 12:20:07 -0500

    Update conexion.php

--- LogicaLogin/conexion.php
+++ LogicaLogin/conexion.php
@@ -5,7 +5,7 @@ $dbpass = "Grupo07#";
 $dbname = "mi-veterinariadb";
 
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, 3306);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn){
     die("No se pudo establecer la conexion: ".mysqli_connect_error());
 }
